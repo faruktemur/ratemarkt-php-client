c73adbce2c9f71570b87e94e9267166c70d11f6c
Faruk Temur <faruk.temur@metglobal.com>
2017-07-18 19:54:33 +0300

    Added options class

---
+++ src/Ratemarkt/Exception/ValidationErrorException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Ratemarkt\Exception;
+
+use ErrorException;
+
+/**
+ * Class ValidationErrorException
+ * @package Ratemarkt\Exception
+ */
+class ValidationErrorException extends ErrorException
+{
+
+}
---
+++ src/Ratemarkt/Options.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Ratemarkt;
+
+/**
+ * Class Options
+ * @package Ratemarkt
+ */
+class Options
+{
+    /**
+     * @var string
+     */
+    private $apiKey;
+
+    /**
+     * @var string
+     */
+    private $secretKey;
+
+    /**
+     * @var string
+     */
+    private $baseUrl;
+
+    /**
+     * @return mixed
+     */
+    public function getApiKey()
+    {
+        return $this->apiKey;
+    }
+
+    /**
+     * @param mixed $apiKey
+     */
+    public function setApiKey($apiKey)
+    {
+        $this->apiKey = $apiKey;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSecretKey()
+    {
+        return $this->secretKey;
+    }
+
+    /**
+     * @param mixed $secretKey
+     */
+    public function setSecretKey($secretKey)
+    {
+        $this->secretKey = $secretKey;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBaseUrl()
+    {
+        return $this->baseUrl;
+    }
+
+    /**
+     * @param mixed $baseUrl
+     */
+    public function setBaseUrl($baseUrl)
+    {
+        $this->baseUrl = $baseUrl;
+    }
+}
